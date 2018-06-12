7ffad757c52f49771ec6fd5d598103de2f6cb9c8
Dimon <Dimon@DESKTOP-RMP3SSA>
2018-06-13 00:03:22 +0300

    1.9

--- Gallery/Form1.cs
+++ Gallery/Form1.cs
@@ -31,31 +31,36 @@ namespace Gallery
         private void UpdateListFiles()
         {
             CurrentPictures = 0;
-            DirectoryInfo InfoDir = new DirectoryInfo(TPath.Text);
-            Pictures.Clear();
-            ListFiles.Items.Clear();
-            foreach (var item in InfoDir.EnumerateFiles())
+
+            if (TPath.Text != "" && Directory.Exists(TPath.Text))
             {
-                if (item.Extension == ".png" || item.Extension == ".jpg" || item.Extension == ".gif")
+                DirectoryInfo InfoDir = new DirectoryInfo(TPath.Text);
+                Pictures.Clear();
+                ListFiles.Items.Clear();
+                foreach (var item in InfoDir.EnumerateFiles())
                 {
-                    ListFiles.Items.Add(item.Name);
-                    Pictures.Add(new PictureInfo { FullName = item.FullName, NamePicture = item.Name });
+                    if (item.Extension == ".png" || item.Extension == ".jpg" || item.Extension == ".gif")
+                    {
+                        ListFiles.Items.Add(item.Name);
+                        Pictures.Add(new PictureInfo { FullName = item.FullName, NamePicture = item.Name });
+                    }
                 }
+
+                if (Pictures.Count > 0)
+                {
+                    Mark1.Visible = Mark2.Visible = Mark3.Visible = Mark4.Visible = Mark5.Visible = true;
+                    TBComment.Visible = LComment.Visible = true;
+                    PictureBox.Image = Image.FromFile(Pictures[0].FullName);
+                    PicturesSave.AddRange(Pictures);
+                }
+                else
+                {
+                    Mark1.Visible = Mark2.Visible = Mark3.Visible = Mark4.Visible = Mark5.Visible = false;
+                    TBComment.Visible = LComment.Visible = false;
+                }
+
+                FilterPicture();
             }
-            if(Pictures.Count > 0)
-            {
-                Mark1.Visible = Mark2.Visible = Mark3.Visible = Mark4.Visible = Mark5.Visible = true;
-                TBComment.Visible = LComment.Visible = true;
-                PictureBox.Image = Image.FromFile(Pictures[0].FullName);
-                PicturesSave.AddRange(Pictures);
-            }
-            else
-            {
-                Mark1.Visible = Mark2.Visible = Mark3.Visible = Mark4.Visible = Mark5.Visible = false;
-                TBComment.Visible = LComment.Visible = false;
-            }
-            
-            FilterPicture();
         }
 
         private void FilterPicture()
@@ -66,10 +71,11 @@ namespace Gallery
 
         private void BNext_Click(object sender, EventArgs e)
         {
-            if (CurrentPictures < Pictures.Count()-1)
+            if (CurrentPictures < Pictures.Count() - 1)
             {
                 CurrentPictures++;
                 PictureBox.Image = Image.FromFile(Pictures[CurrentPictures].FullName);
+                SetValue();
             }
         }
 
@@ -79,6 +85,39 @@ namespace Gallery
             {
                 CurrentPictures--;
                 PictureBox.Image = Image.FromFile(Pictures[CurrentPictures].FullName);
+                SetValue();
+            }
+        }
+
+        private void SetValue()
+        {
+            if (PicturesSave.Exists(x => x.NamePicture == Pictures[CurrentPictures].NamePicture))
+            {
+                var SelItem = PicturesSave.First(x => x == Pictures[CurrentPictures]);
+                this.LComment.Text = SelItem.Comment;
+                switch (SelItem.Mark)
+                {
+                    case 1:
+                        this.Mark1.Checked = true;
+                        break;
+                    case 2:
+                        this.Mark2.Checked = true;
+                        break;
+                    case 3:
+                        this.Mark3.Checked = true;
+                        break;
+                    case 4:
+                        this.Mark4.Checked = true;
+                        break;
+                    case 5:
+                        this.Mark5.Checked = true;
+                        break;
+                }
+            }
+            else
+            {
+                LComment.Text = "";
+                Mark1.Checked = Mark2.Checked = Mark3.Checked = Mark4.Checked = Mark5.Checked = false;
             }
         }
 
